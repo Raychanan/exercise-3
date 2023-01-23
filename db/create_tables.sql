b53e7f8532a2108bc02fe43d127ac8fbd8917681
Trevor Austin <trevor@scenery.video>
2023-01-23 08:49:37 -0600

    Fix primary key column definitions

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,12 +1,12 @@
 create table posts (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   body TEXT
 );
 
 create table comments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  post_id INT,
+  id INTEGER PRIMARY KEY,
+  post_id INTEGER,
   body TEXT,
   author VARCHAR(30),
   FOREIGN KEY(post_id) REFERENCES posts(id)
